d961a3f53ee9c365ff1cc428950872c7d212e289
Antonin <antoriche.ar@gmail.com>
2022-11-01 21:12:36 +0100

    suggestion

---
+++ src/components/SimulationView/SimulationView.js
@@ -0,0 +1,37 @@
+import * as Plot from "@observablehq/plot";
+import { PlotFigure } from "plot-react";
+import React from "react";
+
+const fakedata = [
+  { x: 0, y: 0 },
+  { x: 1, y: 1 },
+  { x: 2, y: 2 },
+  { x: 3, y: 3 },
+  { x: 4, y: 4 },
+  { x: 5, y: 5 },
+];
+
+const SimulationView = ({ simulationId }) => {
+  // TODO : get the simulation from the API
+  // TODO : display the simulation name and description
+  // TODO : display the simulation data in a chart
+
+  return (
+    <div>
+      <h2>SimulationView</h2>
+      <PlotFigure
+        options={{
+          marks: [
+            Plot.line(fakedata, {
+              x: "x",
+              y: "y",
+              stroke: "dodgerblue",
+            }),
+          ],
+        }}
+      />
+    </div>
+  );
+};
+
+export default SimulationView;
